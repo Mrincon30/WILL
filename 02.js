5fc10c2f5a7f0970f61ee30d7c8c16a3dde86ce0
Mariugenia Rincon <mariugeniarincon36@gmail.com>
2022-09-20 21:03:50 -0500

    Resueltos 1 y 2

--- 02.js
+++ 02.js
@@ -12,6 +12,13 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  var larga = strings[0];
+  for (var i = 0; i <strings.length; i++) {
+    if(strings[i].length > larga.length){
+      larga= strings[i];
+    }
+  }
+  return larga;
 }
 
 // No modifiques nada debajo de esta linea //
